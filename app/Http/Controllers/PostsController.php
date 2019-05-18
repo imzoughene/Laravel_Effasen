0a5bdcee0c52b4bf50fe4506e4ae1abe7dbb7eaf
Youssef Imzoughene <amzianx@gmail.com>
2019-05-18 19:00:58 +0000

    Elequent ORM read data

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,6 +14,7 @@ class PostsController extends Controller
     public function index()
     {
         $posts = Post::all();
+        //$posts= [];
         return view('posts.index',compact('posts'));
     }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,10 +4,22 @@
     <div class="title m-b-md">
         Posts
     </div>
-    <ul>
+    @if(count($posts)>0)
         @foreach ($posts as $item)
-          <li>{{ $item->body }}</li>  
+        <div class="card text-white bg-primary">
+        <h5 class="card-header"><b>{{ $item->subject }}</b></h5>
+        <div class="card-body text-left">
+            <h5 class="card-title">{{ $item->firstname.' | '.$item->lastname }}</h5>
+            <span class="badge badge-pill badge-danger">Created at : {{ $item->created_at }}</span>
+            <br>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
         @endforeach
-    </ul>
+    @else
+    <div class="alert alert-dismissible alert-danger">
+        <strong>No posts</strong> 
+    </div>
+    @endif    
 </div>
 @endsection
