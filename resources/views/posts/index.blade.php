fe1a695f9856c765f1fe973f60a6f32d1569dbde
Youssef Imzoughene <amzianx@gmail.com>
2019-05-19 00:04:00 +0000

    Eloquent ORM read data

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,17 +5,23 @@
         Posts
     </div>
     @if(count($posts)>0)
-        @foreach ($posts as $item)
-        <div class="card text-white bg-primary">
-        <h5 class="card-header"><b>{{ $item->subject }}</b></h5>
-        <div class="card-body text-left">
-            <h5 class="card-title">{{ $item->firstname.' | '.$item->lastname }}</h5>
-            <span class="badge badge-pill badge-danger">Created at : {{ $item->created_at }}</span>
-            <br>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="container">
+            <div class="row">
+                @foreach ($posts as $item)
+                <div class="col-sm-4">
+                    <div class="card text-white bg-primary">
+                    <h5 class="card-header"><b>{{ $item->subject }}</b></h5>
+                    <div class="card-body text-left">
+                        <h5 class="card-title">{{ $item->firstname.' | '.$item->lastname }}</h5>
+                        <span class="badge badge-pill badge-danger">Created at : {{ $item->created_at }}</span>
+                        <br>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+                </div>
+                @endforeach      
+            </div>
         </div>
-        </div>
-        @endforeach
     @else
     <div class="alert alert-dismissible alert-danger">
         <strong>No posts</strong> 
